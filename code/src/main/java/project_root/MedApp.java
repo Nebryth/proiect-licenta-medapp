cab8606292c4e5bd9531b0af790929701e80b30d
Nebryth <paltinisanu.paula@gmail.com>
2020-08-09 19:41:35 +0300

    latest

---
+++ code/src/main/java/project_root/MedApp.java
@@ -0,0 +1,11 @@
+package project_root;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedApp {
+    public static void main(String[] args) {
+    SpringApplication.run(MedApp.class, args);
+}
+
+}
